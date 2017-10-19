9aad8827d76983777d9e7894990f8f7f1225dfbd
irwenjing <irwenjing@gmail.com>
2017-10-19 15:11:16 +0800

    10.19

--- 封装复习/review-01/.router.js
+++ 封装复习/review-01/.router.js
@@ -1,17 +1,18 @@
 function setRouter(app){ 
  var router = app; 
 
+//服务端 router.js
+
+
 app.get('/loadMore', function(req, res){
 
 	var curIdx = req.query.index
 	var len = req.query.length
 	var data = []
 
-	for(var i= 0; i<len; i++){
-		data.push('新闻' + (parseInt(index) + i))
+	for(var i=0; i<len; i++){
+		data.push('新闻' + (parseInt(curIdx) + i))
 	}
-	setTimeout(function(){
-		res.send(data);
-	}, 5000)
+	res.send(data)
 })}
  module.exports.setRouter = setRouter
--- 封装复习/review-02/router.js
+++ 封装复习/review-02/router.js
@@ -1,13 +1,17 @@
+//服务端 router.js
+
+
+
 app.get('/loadMore', function(req, res){
 
 	var curIdx = req.query.index
 	var len = req.query.length
 	var data = []
 
-	for(var i= 0; i<len; i++){
-		data.push('新闻' + (parseInt(index) + i))
+	for(var i=0; i<len; i++){
+		data.push('新闻' + (parseInt(curIdx) + i))
 	}
 	setTimeout(function(){
-		res.send(data);
-	}, 5000)
+		res.send(data)
+	}, 3000);
 })
